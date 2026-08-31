db52650113dd973cf6b558c9791679e1fc6184d9
Nick Pickering <npick001@odu.edu>
2026-08-31 09:57:43 -0400

    fixed stupid misspell

--- MekanismReactors/Fission/ReactorController.lua
+++ MekanismReactors/Fission/ReactorController.lua
@@ -3,29 +3,31 @@ require("Control")
 local logs = require("Logging")
 
 function main()
-    -- Initalize Logs
-    logs.SetLogPaths("logs/FissionReactor/errors.log", "logs/FissionReactor/debug.log", "logs/FissionReactor/info.log")
-    logs.OpenLogs()
-    logs.RefreshLogs()
+	-- Initalize Logs
+	logs.SetLogPaths("logs/FissionReactor/errors.log", "logs/FissionReactor/debug.log", "logs/FissionReactor/info.log")
+	logs.OpenLogs()
+	logs.RefreshLogs()
 
-    -- Log Header
-    logs.LogDebug("Activity, Fuel (%), Heat (K), Waste (%), Coolant (%), Heated Coolant (%), Burn Rate (mb/t), Damage (%)")
+	-- Log Header
+	logs.LogDebug(
+		"Activity, Fuel (%), Heat (K), Waste (%), Coolant (%), Heated Coolant (%), Burn Rate (mb/t), Damage (%)"
+	)
 
-    -- Check reactor status
-    for _, perph_name in pairs(peripheral.getNames()) do
-        if peripheral.getType(perph_name) == "fissionReactorLogicAdaptor" then
-            logs.LogInfo("Fission Reactor Logic Adaptor found: " .. perph_name)
-            reactor = peripheral.wrap(perph_name)
-            break
-        end
-    end
-    while true do
-        ControlReactor(reactor, GetReactorStatus(reactor))
-        sleep(1) -- Run once per second
-    end
+	-- Check reactor status
+	for _, perph_name in pairs(peripheral.getNames()) do
+		if peripheral.getType(perph_name) == "fissionReactorLogicAdapter" then
+			logs.LogInfo("Fission Reactor Logic Adapter found: " .. perph_name)
+			reactor = peripheral.wrap(perph_name)
+			break
+		end
+	end
+	while true do
+		ControlReactor(reactor, GetReactorStatus(reactor))
+		sleep(1) -- Run once per second
+	end
 
-    -- Ensure logs are closed on exit
-    logs.CloseLogs()
+	-- Ensure logs are closed on exit
+	logs.CloseLogs()
 end
 
 main()
