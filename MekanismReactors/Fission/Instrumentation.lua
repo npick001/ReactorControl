6a29d9da8054065f8deddd15ba9dc8bbf99afd05
Nick Pickering <npick001@odu.edu>
2026-09-03 15:08:21 -0400

    update to wait for reactor to populate

--- MekanismReactors/Fission/Instrumentation.lua
+++ MekanismReactors/Fission/Instrumentation.lua
@@ -8,46 +8,50 @@
 local RI = {}
 
 function GetReactorStatus(reactor)
-    return {
-        activity = RI.CheckActivity(reactor),
-        fuel = RI.CheckFuel(reactor),
-        heat = RI.CheckFuelHeat(reactor),
-        waste = RI.CheckWaste(reactor),
-        coolant = RI.CheckCoolantLevels(reactor),
-        heated_coolant = RI.CheckHeatedCoolantLevels(reactor),
-        burn_rate = RI.GetCurrentBurnRate(reactor),
-        dmg_percent = RI.GetDmgPercent(reactor),
-    }
+	if reactor == nil then
+		return
+	end
+
+	return {
+		activity = RI.CheckActivity(reactor),
+		fuel = RI.CheckFuel(reactor),
+		heat = RI.CheckFuelHeat(reactor),
+		waste = RI.CheckWaste(reactor),
+		coolant = RI.CheckCoolantLevels(reactor),
+		heated_coolant = RI.CheckHeatedCoolantLevels(reactor),
+		burn_rate = RI.GetCurrentBurnRate(reactor),
+		dmg_percent = RI.GetDmgPercent(reactor),
+	}
 end
 
 function RI.CheckActivity(r)
-    return r.getStatus()
+	return r.getStatus()
 end
 
 function RI.CheckFuel(r)
-    return r.getFuelFilledPercentage() * 100
+	return r.getFuelFilledPercentage() * 100
 end
 
 function RI.CheckWaste(r)
-    return r.getWasteFilledPercentage() * 100
+	return r.getWasteFilledPercentage() * 100
 end
 
 function RI.CheckFuelHeat(r)
-    return r.getTemperature()
+	return r.getTemperature()
 end
 
 function RI.GetCurrentBurnRate(r)
-    return r.getActualBurnRate()    
+	return r.getActualBurnRate()
 end
 
 function RI.GetDmgPercent(r)
-    return r.getDamagePercent()
+	return r.getDamagePercent()
 end
 
 function RI.CheckCoolantLevels(r)
-    return r.getCoolantFilledPercentage() * 100
+	return r.getCoolantFilledPercentage() * 100
 end
 
 function RI.CheckHeatedCoolantLevels(r)
-    return r.getHeatedCoolantFilledPercentage() * 100
+	return r.getHeatedCoolantFilledPercentage() * 100
 end
